8f252a31730f1d005187a9c81c9c45522d601c7d
Marten Hoekstra <marten@dolhuis.nl>
2018-08-13 15:39:36 +0200

    Add inserts of events Fixes #5
    Events weren't registered in the database, but the are supposed to be.
    This commit adds saving them on update.

--- src/gobcore/models/event.py
+++ src/gobcore/models/event.py
@@ -6,7 +6,7 @@ Events are the key data of the database. It allows to (re-)construct the databas
 """
 
 EVENTS = {
-    "eventid": "GOB.Integer",     # Unique identification of the event, numbered sequentially
+    "eventid": "GOB.PKInteger",   # Unique identification of the event, numbered sequentially
     "timestamp": "GOB.DateTime",  # datetime when the event as created
     "entity": "GOB.String",       # the entity to which the event need to be applied
     "action": "GOB.String",       # add, change, delete or confirm
@@ -14,3 +14,17 @@ EVENTS = {
     "source_id": "GOB.String",    # the id of the entity in the source
     "contents": "GOB.JSON"        # a json object that holds the contents for the action, the full entity for an Add
 }
+
+
+def create_event(orm_event, gob_event, data, metadata):
+    """Method to fill the orm event entity with the required data,
+    specifically placed here, to make sure all fields above are filled """
+
+    return orm_event(
+        timestamp=metadata.timestamp,
+        entity=metadata.entity,
+        action=gob_event.name,
+        source=metadata.source,
+        source_id=data[metadata.source_id_column],
+        contents=data
+    )
