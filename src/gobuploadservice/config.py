0eb3e6bd924eeead139e15fe5eaf740d30b399d6
Marten Hoekstra <marten@dolhuis.nl>
2018-09-05 15:44:16 +0200

    Add deployment variables and definition

--- src/gobuploadservice/config.py
+++ src/gobuploadservice/config.py
@@ -2,8 +2,8 @@ import os
 
 GOB_DB = {
     'drivername': 'postgres',
-    'username': 'gob',
-    'password': 'insecure',
-    'host': os.getenv("DB_HOST", "localhost"),
-    'port': 5406
+    'username': os.getenv("DATABASE_USER", "gob"),
+    'password': os.getenv("DATABASE_PASSWORD", "insecure"),
+    'host': os.getenv("DATABASE_HOST_OVERRIDE", "localhost"),
+    'port': os.getenv("DATABASE_PORT_OVERRIDE", 5406),
 }
