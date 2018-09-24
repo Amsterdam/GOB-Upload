c21c38005389168fffb4f5c6c88af269fbe60f58
Jasper Jager <mail@jasperjager.com>
2018-09-24 11:47:46 +0200

    Fix storage issues
    Use the GOBTypeJSONEncoder to fix issues with Decimal. Add the database overrides to the docker-compose for local testing and improve the print output of events.

--- src/gobuploadservice/__init__.py
+++ src/gobuploadservice/__init__.py
@@ -10,6 +10,6 @@ def print_report(events):
     """
     print(f"Aantal mutaties: {len(events)}")
     for gob_event in GOB_EVENTS:
-        events = [event for event in events if event['event'] == gob_event.name]
-        if len(events) > 0:
-            print(f"- {gob_event.name}: {len(events)}")
+        counted_events = [event for event in events if event['event'] == gob_event.name]
+        if len(counted_events) > 0:
+            print(f"- {gob_event.name}: {len(counted_events)}")
