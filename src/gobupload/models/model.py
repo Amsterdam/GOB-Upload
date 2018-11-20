afad18bddd9eacbcd00e27bff20696d9adff4f12
vegter <amsjurien@gmail.com>
2018-11-20 11:44:01 +0100

    Convert GOBModel() to SQLAlchemy Model
    Preparation for alembic migrations
    The SQLAlchemy model will be compared with the DB model
    Migrations will be generated and applied automatically

--- src/gobupload/models/model.py
+++ src/gobupload/models/model.py
@@ -1,45 +1,73 @@
+from sqlalchemy import Index
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import sessionmaker
+
+# Import data definitions
+from gobcore.model import GOBModel
+from gobcore.model.metadata import FIXED_COLUMNS, METADATA_COLUMNS
+from gobupload.storage.db_models.event import EVENTS
+
+# Utility method to convert GOB type to a SQLAlchemy Column
+from gobupload.storage.db_models import get_column
+
+# Store all model classes
+models = {
+    # e.g. "meetbouten_rollagen": <BASE>
+}
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(type_=String)
-    fullname = Column(String)
-    password = Column(String)
-    # extra = Column(String)
+def _derive_models():
 
-    def __repr__(self):
-        return "<User(name='%s', fullname='%s', password='%s')>" % (
-            self.name, self.fullname, self.password)
+    def columns_to_model(table_name, columns):
+        # Convert columns to SQLAlchemy Columns
+        columns = {column_name: get_column((column_name, column_type)) for column_name, column_type in columns.items()}
 
-test_entity = {
-    "name": "test_entity",
-    "version": "0.1",
-    "entity_id": "testid",
-    "attributes": {
-        "string": {
-            "type": "String",
-            "description": "String value."
-        }
-    }
-}
+        # Create model
+        models[table_name] = type(table_name, (Base,), {
+            "__tablename__": table_name,
+            **columns,
+            "__repr__": lambda self: f"{table_name}"
+        })
 
-def get_type():
-    return String
+    model = GOBModel()
 
-def get_column(type):
-    return Column(type)
+    # Start with events
+    columns_to_model("events", EVENTS)
 
-types = {}
-name = "Department"
-types[name] = type(name, (Base,), {
-    "__tablename__": test_entity["name"],
-    "id": Column(Integer, primary_key=True),
-    "string": get_column(get_type()),
-    "__repr__": lambda self: f"Entity {test_entity['name']}"
-})
+    for catalog_name, catalog in model.get_catalogs().items():
+        for collection_name, collection in model.get_collections(catalog_name).items():
+            if collection['version'] != "0.1":
+                # No migrations defined yet...
+                raise ValueError("Unexpected version, please write a generic migration here or migrate the import")
+
+            # "attributes": {
+            #     "attribute_name": {
+            #         "type": "GOB type name, e.g. GOB.String",
+            #         "description": "attribute description",
+            #         "ref: "collection_name:entity_name, e.g. meetbouten:meetbouten"
+            #     }, ...
+            # }
+
+            # the GOB model for the specified entity
+            fields = {col: desc['type'] for col, desc in collection['fields'].items()}
+
+            # Collect all columns for this collection
+            entity = {
+                **FIXED_COLUMNS,
+                **METADATA_COLUMNS['private'],
+                **METADATA_COLUMNS['public'],
+                **fields
+            }
+
+            table_name = model.get_table_name(catalog_name, collection_name)
+            columns_to_model(table_name, entity)
+            Index(
+                f'{table_name}.idx.source_source_id',
+                '_source',
+                '_source_id',
+                unique=True
+            )
+
+
+_derive_models()
