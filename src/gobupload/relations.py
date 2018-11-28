12261ca29e6ec383d2f46a236a7d9d09915db45a
Jasper Jager <mail@jasperjager.com>
2018-11-28 17:55:57 +0100

    Chained relations added to relate functions

--- src/gobupload/relations.py
+++ src/gobupload/relations.py
@@ -19,6 +19,12 @@ sources = GOBSources()
 storage = None
 
 
+# TODO: see if alignment with sqlalchemy is possible
+OPERATORS = {
+    'is_null': 'IS NULL'
+}
+
+
 def build_relations(msg):
     """Build all relations for a catalog.
 
@@ -87,14 +93,17 @@ def _get_all_entities(update_model, relation):
         )
 
 
-def _equals(update_table, source_table, relation):
+def _equals(catalog_name, collection_name, relation):
     """Build an update query based on a direct relation between 'bronwaarde' and a field in the related object
 
-    :param update_table: the table_name of the collection we are about to update
-    :param source_table: the table_name of the collection we relating to
+    :param catalog_name: the source catalog we are relating to
+    :param collection_name: the source collection we are relating to
     :param relation: the relation dict, containing all details about the relation
     :return:
     """
+    update_table = model.get_table_name(relation['catalog'], relation['collection'])
+    source_table = model.get_table_name(catalog_name, collection_name)
+
     if relation['type'] == 'GOB.Reference':
         return f"""
 UPDATE {update_table}
@@ -134,6 +143,72 @@ def _geo_in(catalog_name, collection_name, relation):
     return None
 
 
+def _through(catalog_name, collection_name, relation):
+    """Build an update query based on a chain through entities
+
+    :param update_table: the table_name of the collection we are about to update
+    :param source_table: the table_name of the collection we relating to
+    :param relation: the relation dict, containing all details about the relation
+    :return:
+    """
+    if relation['type'] == 'GOB.ManyReference':
+        # Not implemented yet, return None to skip query execution.
+        return None
+
+    update_table = model.get_table_name(relation['catalog'], relation['collection'])
+
+    # Get the last table in the chain
+    final_table = model.get_table_name(*relation['chain'][-1]['entity'].split(':'))
+
+    # Build the joins and filters using the chains in the relation
+    joins, filters = _expand_joins_and_filters(update_table, relation['chain'])
+
+    joins_str = ' '.join(joins)
+
+    query = f"""
+UPDATE {update_table}
+SET {relation['field_name']} = enhanced.related
+FROM (
+    SELECT {update_table}._id, (\'{{"id": "\'|| {final_table}._id ||\'"}}\')::JSONB as related
+    FROM {update_table}
+    {joins_str}
+"""
+    if filters:
+        filters_str = ' AND '.join(filters)
+        query += f"""
+    WHERE {filters_str}
+"""
+    query += f"""
+) AS enhanced
+WHERE {update_table}._source = '{relation['source']}'
+AND {update_table}._id = enhanced._id
+"""
+    return query
+
+
+def _expand_joins_and_filters(update_table, chain):
+    joins = []
+    filters = []
+
+    # Start with the update_table in the chain
+    chain_table = update_table
+    for item in chain:
+        chain_catalog, chain_collection = item['entity'].split(':')
+        # Set the source_table to the next item in the chain
+        source_table = model.get_table_name(chain_catalog, chain_collection)
+        joins.append(f"LEFT JOIN {source_table} ON \
+            {chain_table}.{item['source_attribute']}->>'id' = {source_table}._id")
+        # Set the chain_table to the item we've just handled
+        chain_table = model.get_table_name(chain_catalog, chain_collection)
+
+        # Add the filters
+        if 'filters' in item:
+            for filter in item['filters']:
+                filters.append(f"{chain_table}.{filter['field']} {OPERATORS[filter['op']]}")
+
+    return joins, filters
+
+
 def _get_update_query(catalog_name, collection_name, relation):
     """Build an update query based on a direct relation between 'bronwaarde' and a field in the related object
 
@@ -142,11 +217,10 @@ def _get_update_query(catalog_name, collection_name, relation):
     :param relation: the relation dict, containing all details about the relation
     :return:
     """
-    update_table = model.get_table_name(relation['catalog'], relation['collection'])
-    source_table = model.get_table_name(catalog_name, collection_name)
 
     methods = {
         'equals': _equals,
         'geo_in': _geo_in,
+        'through': _through,
     }
-    return methods[relation['method']](update_table, source_table, relation)
+    return methods[relation['method']](catalog_name, collection_name, relation)
--- src/tests/test_relations.py
+++ src/tests/test_relations.py
@@ -21,8 +21,8 @@ class TestRelations(TestCase):
 
         self.mock_sources = GOBSources()
         self.mock_relation = {
-            'catalog': 'catalog',
-            'collection': 'collection',
+            'catalog': 'catalog2',
+            'collection': 'collection2',
             'source': 'source',
             'field_name': 'field_name',
             'type': 'GOB.Reference',
@@ -30,9 +30,23 @@ class TestRelations(TestCase):
             'destination_attribute': 'identificatie'
         }
 
+        self.mock_chained_relation = {
+            'catalog': 'catalog2',
+            'collection': 'collection2',
+            'source': 'source',
+            'field_name': 'field_name',
+            'type': 'GOB.Reference',
+            'method': 'through',
+            'destination_attribute': 'identificatie',
+            'chain': [
+                {'entity': 'catalog2:collection2', 'source_attribute': 'field2', 'destination_attribute': 'identificatie'},
+                {'entity': 'catalog3:collection3', 'source_attribute': 'field3', 'destination_attribute': 'identificatie'},
+            ]
+        }
+
         self.mock_many_relation = {
-            'catalog': 'catalog',
-            'collection': 'collection',
+            'catalog': 'catalog2',
+            'collection': 'collection2',
             'source': 'source',
             'field_name': 'field_name',
             'type': 'GOB.ManyReference',
@@ -75,12 +89,12 @@ class TestRelations(TestCase):
         self.mock_entities.count.assert_called()
 
         expected_query = f"""
-UPDATE catalog_collection
+UPDATE catalog2_collection2
 SET field_name = field_name::JSONB ||
                                ('{{\"id\": \"'|| catalog_collection._id ||'\"}}')::JSONB
 FROM catalog_collection
 WHERE field_name->>'bronwaarde' = catalog_collection.identificatie
-AND catalog_collection._source = 'source'
+AND catalog2_collection2._source = 'source'
 """
         mock_storage.session.execute.assert_called_with(expected_query)
 
@@ -101,46 +115,67 @@ AND catalog_collection._source = 'source'
         mock_storage.session.query.assert_called()
 
     def test_equals_for_reference(self):
-        query = relations._equals('catalog_collection', 'catalog2_collection2', self.mock_relation)
+        query = relations._equals('catalog', 'collection', self.mock_relation)
 
         expected_query = f"""
-UPDATE catalog_collection
+UPDATE catalog2_collection2
 SET field_name = field_name::JSONB ||
-                               ('{{\"id\": \"'|| catalog2_collection2._id ||'\"}}')::JSONB
-FROM catalog2_collection2
-WHERE field_name->>'bronwaarde' = catalog2_collection2.identificatie
-AND catalog_collection._source = 'source'
+                               ('{{\"id\": \"'|| catalog_collection._id ||'\"}}')::JSONB
+FROM catalog_collection
+WHERE field_name->>'bronwaarde' = catalog_collection.identificatie
+AND catalog2_collection2._source = 'source'
 """
 
         self.assertEqual(query, expected_query)
 
     def test_equals_for_many_reference(self):
-        query = relations._equals('catalog_collection', 'catalog2_collection2', self.mock_many_relation)
+        query = relations._equals('catalog', 'collection', self.mock_many_relation)
 
-        expected_query = f"""
-UPDATE catalog_collection
+        expected_query = """
+UPDATE catalog2_collection2
 SET field_name = enhanced.related
 FROM (
-    SELECT catalog_collection._id, jsonb_agg(value::JSONB ||
-                               ('{{\"id\": \"'|| catalog2_collection2._id ||'\"}}')::JSONB) as related
-    FROM catalog_collection, json_array_elements(catalog_collection.field_name)
-    LEFT JOIN catalog2_collection2
-    ON value->>'bronwaarde' = catalog2_collection2.identificatie
-    GROUP BY catalog_collection._id
+    SELECT catalog2_collection2._id, jsonb_agg(value::JSONB ||
+                               ('{"id": "'|| catalog_collection._id ||'"}')::JSONB) as related
+    FROM catalog2_collection2, json_array_elements(catalog2_collection2.field_name)
+    LEFT JOIN catalog_collection
+    ON value->>'bronwaarde' = catalog_collection.identificatie
+    GROUP BY catalog2_collection2._id
 ) AS enhanced
-WHERE catalog_collection._source = 'source'
-AND catalog_collection._id = enhanced._id
+WHERE catalog2_collection2._source = 'source'
+AND catalog2_collection2._id = enhanced._id
 """
+        self.assertEqual(query, expected_query)
 
     def test_geo_in(self):
-        relations._geo_in('catalog_collection', 'catalog2_collection2', self.mock_many_relation)
+        relations._geo_in('catalog', 'collection', self.mock_many_relation)
+
+    def test_through_for_reference(self):
+        query = relations._through('catalog', 'collection', self.mock_chained_relation)
+
+        expected_query = """
+UPDATE catalog2_collection2
+SET field_name = enhanced.related
+FROM (
+    SELECT catalog2_collection2._id, ('{"id": "'|| catalog3_collection3._id ||'"}')::JSONB as related
+    FROM catalog2_collection2
+    LEFT JOIN catalog2_collection2 ON             catalog2_collection2.field2->>'id' = catalog2_collection2._id LEFT JOIN catalog3_collection3 ON             catalog2_collection2.field3->>'id' = catalog3_collection3._id
+
+) AS enhanced
+WHERE catalog2_collection2._source = 'source'
+AND catalog2_collection2._id = enhanced._id
+"""
+        self.assertEqual(query, expected_query)
+
+    def test_through_for_many_reference(self):
+        relations._through('catalog', 'collection', self.mock_many_relation)
 
     @patch('gobupload.relations._geo_in')
     @patch('gobupload.relations._equals')
     def test_get_update_query(self, mock_equals, mock_geo_in):
         relations._get_update_query('catalog', 'collection', self.mock_relation)
-        mock_equals.assert_called_with('catalog_collection', 'catalog_collection', self.mock_relation)
+        mock_equals.assert_called_with('catalog', 'collection', self.mock_relation)
 
         self.mock_relation['method'] = 'geo_in'
         relations._get_update_query('catalog', 'collection', self.mock_relation)
-        mock_geo_in.assert_called_with('catalog_collection', 'catalog_collection', self.mock_relation)
+        mock_geo_in.assert_called_with('catalog', 'collection', self.mock_relation)
