97e94014dd0f33b10677668e9639b06c814d02c2
vegter <amsjurien@gmail.com>
2019-03-19 08:28:39 +0100

    Update actual relations
    Apply relations on current model

---
+++ src/tests/relate/test_publish.py
@@ -0,0 +1,70 @@
+from unittest import TestCase, mock
+from unittest.mock import MagicMock, patch
+
+from gobupload.relate.publish import publish_relations
+
+@patch('gobupload.relate.publish.logger', MagicMock())
+class TestInit(TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @patch('gobupload.relate.publish.publish')
+    def test_publish_empty_relations(self, mocked_publish):
+        msg = {
+            "header": {}
+        }
+        publish_relations(msg, [], False, False)
+        mocked_publish.assert_called()
+
+    @patch('gobupload.relate.publish.publish')
+    def test_publish_relations(self, mocked_publish):
+        relations = [
+            {
+                "src": {
+                    "source": "src_source",
+                    "id": "src_id",
+                    "volgnummer": "src_volgnummer"
+                },
+                "begin_geldigheid": "begin",
+                "eind_geldigheid": "eind",
+                "dst": [{
+                    "source": "dst_source",
+                    "id": "dst_id",
+                    "volgnummer": "dst_volgnummer"
+                }]
+            }
+        ]
+        msg = {
+            "header": {}
+        }
+        expect = {
+            'header': {},
+            'summary': {
+                'num_records': 1
+            },
+            'contents': [{
+                'source': 'src_source.dst_source',
+                'id': 'src_id.src_volgnummer.dst_id.dst_volgnummer',
+                'src_source': 'src_source',
+                'src_id': 'src_id',
+                'src_volgnummer': 'src_volgnummer',
+                'derivation': 'key',
+                'begin_geldigheid': 'begin',
+                'eind_geldigheid': 'eind',
+                'dst_source': 'dst_source',
+                'dst_id': 'dst_id',
+                'dst_volgnummer': 'dst_volgnummer',
+                '_source_id': 'src_id.src_volgnummer.dst_id.dst_volgnummer'
+            }]
+        }
+        publish_relations(msg, relations.copy(), False, False)
+        mocked_publish.assert_called_with('gob.workflow.proposal', 'fullimport.proposal', expect)
+
+        expect["contents"][0]["id"] = expect["contents"][0]["id"] + ".begin"
+        expect["contents"][0]["_source_id"] = expect["contents"][0]["id"]
+        publish_relations(msg, relations.copy(), False, True)
+        mocked_publish.assert_called_with('gob.workflow.proposal', 'fullimport.proposal', expect)
