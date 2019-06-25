31cf726e6c2d28dd6b84bf79fcd5fe687b9b02fe
vegter <amsjurien@gmail.com>
2019-06-25 13:37:31 +0200

    Update query for relations

--- src/tests/storage/test_relate.py
+++ src/tests/storage/test_relate.py
@@ -4,7 +4,7 @@ from unittest.mock import MagicMock, patch
 from gobcore.model import GOBModel
 from gobcore.sources import GOBSources
 
-from gobupload.storage.relate import get_relations, _get_data, get_last_change, get_current_relations, RelationUpdater, _query_missing, check_relations
+from gobupload.storage.relate import get_relations, update_relations, EQUALS, LIES_IN, JOIN, WHERE, _update_match, _get_data, get_last_change, get_current_relations, RelationUpdater, _query_missing, check_relations
 
 @patch('gobupload.relate.relate.logger', MagicMock())
 class TestRelations(TestCase):
@@ -375,3 +375,125 @@ ORDER BY
             check_relations("any_catalog", "any_collection", "any_field name")
         mock_missing.assert_called()
         self.assertEqual(mock_missing.call_count, 3)
+
+    def test_update_match(self):
+        spec = {
+            'method': EQUALS,
+            'destination_attribute': "any_dst_attr"
+        }
+        field = "any_field"
+        stmt = _update_match(spec, field, JOIN)
+        self.assertEqual(stmt, "dst.any_dst_attr = any_field ->> 'bronwaarde'")
+
+        stmt = _update_match(spec, field, WHERE)
+        self.assertEqual(stmt, "any_field IS NOT NULL AND any_field ->> 'bronwaarde' IS NOT NULL")
+
+    @patch('gobupload.storage.relate._geo_resolve', lambda x, y: (x, y))
+    def test_update_geo_match(self):
+        spec = {
+            'method': LIES_IN,
+            'destination_attribute': "any_dst_attr"
+        }
+        field = "any_field"
+        s, q = _update_match(spec, field, "any query type")
+        self.assertEqual(s, spec)
+        self.assertEqual(q, "any query type")
+
+    @patch('gobupload.storage.relate._execute')
+    def test_update_relations(self, mock_execute):
+        mock._execute = MagicMock()
+        mock_get_collection = lambda *args: {
+            'all_fields': {
+                'field': {
+                    'type': 'GOB.ManyReference',
+                    'ref': 'dst_catalogue:dst_collection'
+                }
+            }
+        }
+        mock_get_field_relations = lambda *args: [
+            {
+                'source': 'src_application',
+                'field_name': 'src_attr',
+                'destination_attribute': 'dst_attr',
+                'method': 'equals'
+            }
+        ]
+        expect = """
+UPDATE
+    catalog_collection src
+SET
+    field = new_vals.field_updated
+FROM (
+    
+    SELECT
+        src__id, src_volgnummer,
+        array_to_json(array_agg(field_updated_elm)) field_updated
+    FROM (
+
+        SELECT
+            src__id, src_volgnummer,
+            src_matchcolumn,
+            dst__id, max(dst_volgnummer) dst_volgnummer,
+            jsonb_set(
+jsonb_set(src_matchcolumn, '{volgnummer}', COALESCE(to_jsonb(max(dst_volgnummer)), 'null'::JSONB))
+, '{id}', COALESCE(to_jsonb(dst__id::TEXT), 'null'::JSONB))
+                field_updated_elm
+        FROM (
+SELECT
+    CASE
+    WHEN src._application = 'src_application' THEN 'equals' END AS method,
+    CASE
+    WHEN src._application = 'src_application' THEN dst.dst_attr END AS match,
+    json_arr_elm AS src_matchcolumn,
+    src._date_deleted AS src__date_deleted,
+    src._source AS src__source,
+    src._id AS src__id,
+    src.volgnummer AS src_volgnummer,
+    src.begin_geldigheid AS src_begin_geldigheid,
+    src.eind_geldigheid AS src_eind_geldigheid,
+    dst._date_deleted AS dst__date_deleted,
+    dst._source AS dst__source,
+    dst._id AS dst__id,
+    dst.volgnummer AS dst_volgnummer,
+    dst.begin_geldigheid AS dst_begin_geldigheid,
+    dst.eind_geldigheid AS dst_eind_geldigheid,
+    dst.dst_attr AS dst_match_dst_attr
+FROM catalog_collection AS src
+
+JOIN jsonb_array_elements(src.field) AS json_arr_elm ON TRUE
+
+LEFT OUTER JOIN (
+SELECT
+    _date_deleted,
+    _source,
+    _id,
+    volgnummer,
+    begin_geldigheid,
+    eind_geldigheid,
+    dst_attr
+FROM dst_catalogue_dst_collection) AS dst
+ON
+    (src._application = 'src_application' AND dst.dst_attr = json_arr_elm ->> 'bronwaarde') AND
+    (dst.begin_geldigheid <= src.eind_geldigheid OR src.eind_geldigheid IS NULL) AND
+    (dst.eind_geldigheid >= src.begin_geldigheid OR dst.eind_geldigheid IS NULL)
+WHERE
+    (src._application = 'src_application' AND json_arr_elm IS NOT NULL AND json_arr_elm ->> 'bronwaarde' IS NOT NULL) AND
+    (src._date_deleted IS NULL AND dst._date_deleted IS NULL)
+ORDER BY
+    src._source, src._id, src.volgnummer::int, src.begin_geldigheid, dst.begin_geldigheid, dst.eind_geldigheid
+) relations
+GROUP BY src__id, src_volgnummer, src_matchcolumn, dst__id
+
+    ) last_dsts
+    GROUP BY src__id, src_volgnummer
+
+) new_vals
+WHERE
+    new_vals.src__id = src._id AND new_vals.src_volgnummer = src.volgnummer;
+"""
+        with patch.object(GOBSources, 'get_field_relations', mock_get_field_relations), \
+             patch.object(GOBModel, 'get_collection', mock_get_collection), \
+             patch.object(GOBModel, 'has_states', lambda *args: True):
+            update_relations("catalog", "collection", "field")
+        mock_execute.assert_called_with(expect)
+
