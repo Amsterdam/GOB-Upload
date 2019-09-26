a42a700e5c0f6a2a8058b31b767a2134eb77a845
Jaap Koetsier <jaap@chariot-it.nl>
2019-09-26 14:29:24 +0200

    Fix relate per collection

--- src/tests/relate/test_init.py
+++ src/tests/relate/test_init.py
@@ -17,14 +17,39 @@ class TestInit(TestCase):
         with self.assertRaises(AssertionError):
             build_relations({})
 
-    @patch('gobupload.relate.GOBModel', MagicMock())
-    def test_build_relations(self):
+    @patch('gobupload.relate.GOBModel')
+    @patch('gobupload.relate._build_relations_for_collections')
+    def test_build_relations_header(self, mock_build_for_collections, mock_gob_model):
         result = build_relations({'header': {'catalogue': 'any catalogue'}})
         self.assertEqual(result, {
             'header': mock.ANY,
             'summary': {'warnings': mock.ANY, 'errors': []},
             'contents': []
         })
+
+        model_instance = mock_gob_model.return_value
+
+        model_instance.get_collection_names.assert_called_with('any catalogue')
+        mock_build_for_collections.assert_called_with(
+            'any catalogue',
+            model_instance.get_collection_names.return_value,
+            model_instance,
+        )
+        mock_gob_model.reset_mock()
+
+        result = build_relations({'header': {'catalogue': 'any catalogue', 'collection': 'any collection'}})
+        self.assertEqual(result, {
+            'header': mock.ANY,
+            'summary': {'warnings': mock.ANY, 'errors': []},
+            'contents': []
+        })
+
+        mock_gob_model.return_value.get_collection_names.assert_not_called()
+        mock_build_for_collections.assert_called_with(
+            'any catalogue',
+            ['any collection'],
+            model_instance,
+        )
 
     def test_needs_update(self):
         result = _relation_needs_update("catalog", "collection", "reference", {"ref": "dst_cat:dst_col"})
