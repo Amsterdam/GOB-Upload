497c2585901d569d1326c78d37f474665d5225c4
vegter <amsjurien@gmail.com>
2020-02-25 17:53:16 +0100

    Partition events also on source
    Include a RETURNING statement on events insert

--- src/alembic/versions/ea556acbed92_events_partitioning.py
+++ src/alembic/versions/ea556acbed92_events_partitioning.py
@@ -20,12 +20,12 @@ depends_on = None
 def upgrade():
     # Save existing events by renaming events table
     op.execute("""
+DROP  TABLE IF EXISTS        events_org CASCADE;
 ALTER TABLE events RENAME TO events_org;
 """)
 
     # Create new partitioned events table
     # Primary keys are supported on partitioned tables, foreign keys referencing partitioned tables are not supported.
-    # Unique constraints on partitioned tables must include all the partition key columns
     op.execute("""
 CREATE TABLE public.events (
     eventid     SERIAL    NOT NULL,
@@ -38,21 +38,20 @@ CREATE TABLE public.events (
     source_id   VARCHAR,
     contents    JSONB     NOT NULL,
     application VARCHAR   NOT NULL,
-    PRIMARY KEY (eventid, catalogue, entity),
-    CONSTRAINT events_eventid_catalogue_entity UNIQUE (eventid, catalogue, entity)
+    PRIMARY KEY (eventid, catalogue, entity, source)
 ) PARTITION BY LIST (catalogue);
 """)
 
     # Create events partitions in a separate schema
     op.execute("""
-DROP SCHEMA IF EXISTS events_parts CASCADE;
-CREATE SCHEMA         events_parts;
+DROP SCHEMA IF EXISTS events CASCADE;
+CREATE SCHEMA         events;
 """)
 
     # Create a function to automatically create partitions and insert new events
     op.execute("""
-DROP FUNCTION IF EXISTS insertIntoEventsPartition CASCADE;
-CREATE FUNCTION         insertIntoEventsPartition (
+DROP FUNCTION IF EXISTS insertIntoEvents CASCADE;
+CREATE FUNCTION         insertIntoEvents (
     ev RECORD
 )
 RETURNS VOID
@@ -62,13 +61,16 @@ DECLARE
     schema_name TEXT;
     cat_part    TEXT;
     ent_part    TEXT;
+    src_part    TEXT;
 BEGIN
-    -- Construct catalogue and entity parttion table names
-    schema_name := 'events_parts';
+    -- Construct catalogue and entity partition table names
+    schema_name := 'events';
     -- catalogue partition: event_parts.events_catalogue
     cat_part    := schema_name || '.events_' || ev.catalogue;
     -- entity partition:    event_parts.events_catalogue_entity
     ent_part    := cat_part || '_' || ev.entity;
+    -- source partition:    event_parts.events_catalogue_entity_source
+    src_part    := ent_part || '_' || lower(ev.source);
     -- Create catalogue partition if not yet exists
     IF to_regclass(cat_part) IS NULL THEN
         -- CREATE TABLE cat_part PARTITION OF events FOR VALUES IN (catalogue) PARTITION BY LIST (entity)
@@ -80,17 +82,26 @@ BEGIN
     END IF;
     -- Create entity partition exists if not yet exists
     IF to_regclass(ent_part) IS NULL THEN
-        -- CREATE TABLE ent_part PARTITION OF cat_part FOR VALUES IN (entity)
+        -- CREATE TABLE ent_part PARTITION OF cat_part FOR VALUES IN (entity) PARTITION BY LIST (source)
         EXECUTE 'CREATE TABLE IF NOT EXISTS ' ||
                 ent_part ||
                 ' PARTITION OF ' || cat_part || ' FOR VALUES in (' ||
                 quote_literal(ev.entity) ||
+                ') PARTITION BY LIST(source)';
+    END IF;
+    -- Create source partition exists if not yet exists
+    IF to_regclass(src_part) IS NULL THEN
+        -- CREATE TABLE src_part PARTITION OF ent_part FOR VALUES IN (source)
+        EXECUTE 'CREATE TABLE IF NOT EXISTS ' ||
+                src_part ||
+                ' PARTITION OF ' || ent_part || ' FOR VALUES in (' ||
+                quote_literal(ev.source) ||
                 ')';
     END IF;
-    -- Insert event in the corresponding catalogue-entity partition
-    -- INSERT INTO ent_part (...) VALUES () 
+    -- Insert event in the corresponding catalogue-entity-source partition
+    -- INSERT INTO src_part (...) VALUES () RETURNING ()
     EXECUTE 'INSERT INTO ' ||
-            ent_part || ' ' ||
+            src_part || ' ' ||
             '(' || 
             'eventid, ' ||
             '"timestamp", ' ||
@@ -114,6 +125,18 @@ BEGIN
             coalesce(quote_literal(ev.source_id), 'NULL') || ', ' ||
             quote_literal(ev.contents)    || ', ' ||
             quote_literal(ev.application) ||
+            ') ' ||
+            'RETURNING (' ||
+            ev.eventid                    || ', ' ||
+            quote_literal(ev.timestamp)   || ', ' ||
+            quote_literal(ev.catalogue)   || ', ' ||
+            quote_literal(ev.entity)      || ', ' ||
+            quote_literal(ev.version)     || ', ' ||
+            quote_literal(ev.action)      || ', ' ||
+            quote_literal(ev.source)      || ', ' ||
+            coalesce(quote_literal(ev.source_id), 'NULL') || ', ' ||
+            quote_literal(ev.contents)    || ', ' ||
+            quote_literal(ev.application) ||
             ')';
 END;
 $body$ LANGUAGE plpgsql;
@@ -121,13 +144,13 @@ $body$ LANGUAGE plpgsql;
 
     # Call the Insert-into-events-partition function on every insert into the events table
     op.execute("""
-DROP RULE IF EXISTS autoCall_insertIntoEventsPartition ON events;
-CREATE RULE         autoCall_insertIntoEventsPartition
+DROP RULE IF EXISTS autoCall_insertIntoEvents ON events;
+CREATE RULE         autoCall_insertIntoEvents
 AS
     ON INSERT
     TO events
     DO INSTEAD (
-        SELECT insertIntoEventsPartition(NEW);
+        SELECT insertIntoEvents(NEW);
     );
 """)
 
@@ -137,14 +160,14 @@ INSERT INTO events SELECT * FROM events_org;
 """)
 
     # Create indexes
-    # Improve find distinct source - catalogue - entity combinations
+
     # Quickly find last eventid
-    # Quickly find last action for a specific entity
     op.execute("""
-DROP INDEX IF EXISTS events_source;
-CREATE INDEX         events_source  ON events ("source");
 DROP INDEX IF EXISTS events_eventid;
 CREATE INDEX         events_eventid ON events (eventid);
+""")
+    # Quickly find last action for a specific entity
+    op.execute("""
 DROP INDEX IF EXISTS events_action;
 CREATE INDEX         events_action  ON events ("action");
 """)
