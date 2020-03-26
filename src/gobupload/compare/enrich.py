5a66ac31a2f16bc1db4a4b06b334466b76f1eb90
vegter <amsjurien@gmail.com>
2020-03-26 15:36:27 +0100

    Improve autoid
    Fixed some issues that appeared while making the end-2-end tests

--- src/gobupload/compare/enrich.py
+++ src/gobupload/compare/enrich.py
@@ -126,6 +126,48 @@ AND   {FIELD.END_VALIDITY} IS NULL
     return str(result), None
 
 
+class AutoIdException(Exception):
+
+    def __init__(self, data, specs, column, assigned):
+        """
+        Exception to raise when an entity with ID
+        conflicts with an ID that has previously been issued to an entity without an ID
+
+        :param data:
+        :param specs:
+        :param column:
+        :param assigned:
+        """
+        value = data[column]    # Conflicting (auto-)id value
+        on = specs['on']        # On what field is the auto-id issuing based
+        # Find the entity that has already been assigned the conflicting value
+        assigned_on = [k for k, v in assigned[column]['issued'].items() if v == value][0]
+        # Provide for enough information to solve the issue
+        msg = f"Auto-ID value {value} for entity with {on} = {data[on]} " +\
+              f"already assigned to entity with {on} = {assigned_on}"
+        super().__init__(msg)
+
+
+def _update_last_assigned(data, specs, column, assigned):
+    """
+    Use the template to recognize id values that have the same format as an autoid
+    Adjust the last issued autoid value if required
+
+    :param data:
+    :param specs:
+    :param column:
+    :param assigned:
+    :return:
+    """
+    autoid_pattern = specs['template'].replace("X", r"\d")
+    id = str(data[column])
+    if re.match(autoid_pattern, id):
+        # id is in the format of an auto-id value
+        if not assigned[column]['last'] or (int(id) >= int(assigned[column]['last'])):
+            # and no auto-id has yet been issued or the id is bigger that the last issued auto-id
+            assigned[column]['last'] = id
+
+
 def _autoid(storage, data, specs, column, assigned):
     """
     Auto add id if missing
@@ -137,8 +179,15 @@ def _autoid(storage, data, specs, column, assigned):
     :param assigned: Any already assigned values
     :return:
     """
-    if data.get(column) is not None:
+    column_value = data.get(column)
+    if column_value is not None:
         # Do not overwrite if a value for column already exists
+        if column_value in assigned[column]["issued"].values():
+            # Signal any conflicts with previously assigned values
+            # All handling is yielded from one step to another
+            # It is unfortunately not possible to re-assign the conflicting entity with a new code
+            raise AutoIdException(data, specs, column, assigned)
+        _update_last_assigned(data, specs, column, assigned)
         return data[column], None
 
     on = specs["on"]    # On which column should be searched for an existing value
