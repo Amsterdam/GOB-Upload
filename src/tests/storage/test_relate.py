81024bdedaf4d63121f60740e7e9440879f064a9
Jasper Jager <mail@jasperjager.com>
2020-04-14 13:58:32 +0200

    Disable JSON relations
    Don’t update the relation JSON fields when building relations. The relation tables are used for relations in the REST and GraphQL API

--- src/tests/storage/test_relate.py
+++ src/tests/storage/test_relate.py
@@ -2,14 +2,13 @@ from unittest import TestCase, mock
 from unittest.mock import MagicMock, patch, call
 
 from gobcore.model import GOBModel
-from gobcore.sources import GOBSources
 
 from gobupload.relate.exceptions import RelateException
-from gobupload.storage.relate import update_relations, EQUALS, LIES_IN, JOIN, WHERE, _update_match, \
+from gobupload.storage.relate import EQUALS, LIES_IN, JOIN, WHERE, \
     _get_data, get_last_change, get_current_relations, RelationUpdater, _query_missing, check_relations, \
-    check_very_many_relations, _get_updated_row_count, check_relation_conflicts, _get_relation_check_query
+    check_very_many_relations, check_relation_conflicts, _get_relation_check_query
 
-@patch('gobupload.relate.relate.logger', MagicMock())
+
 class TestRelations(TestCase):
 
     def setUp(self):
@@ -206,188 +205,6 @@ ORDER BY _source, _id, volgnummer, begin_geldigheid
             call(conflicts_msg, expected[0]),
             call(f"{conflicts_msg}: 2 found, 1 reported"),
         ])
-
-
-
-    def test_update_match(self):
-        spec = {
-            'method': EQUALS,
-            'destination_attribute': "any_dst_attr"
-        }
-        field = "any_field"
-        stmt = _update_match(spec, field, JOIN)
-        self.assertEqual(stmt, "dst.any_dst_attr = any_field ->> 'bronwaarde'")
-
-        stmt = _update_match(spec, field, WHERE)
-        self.assertEqual(stmt, "any_field IS NOT NULL AND any_field ->> 'bronwaarde' IS NOT NULL")
-
-    @patch('gobupload.storage.relate._geo_resolve', lambda x, y: (x, y))
-    def test_update_geo_match(self):
-        spec = {
-            'method': LIES_IN,
-            'destination_attribute': "any_dst_attr"
-        }
-        field = "any_field"
-        s, q = _update_match(spec, field, "any query type")
-        self.assertEqual(s, spec)
-        self.assertEqual(q, "any query type")
-
-    @patch('gobupload.storage.relate.logger', MagicMock())
-    @patch('gobupload.storage.relate.get_relation_name')
-    @patch('gobupload.storage.relate._execute')
-    @patch('gobupload.storage.relate._get_data')
-    def test_update_relations(self, mock_get_data, mock_execute, mock_get_relation_name):
-        class MockExecute:
-            def __init__(self, rowcount):
-                self.rowcount = rowcount
-
-        mock_get_relation_name.return_value = 'cat_col_cat2_col2_field'
-
-        mock_execute.return_value = MockExecute(0)
-        mock_get_collection = lambda *args: {
-            'all_fields': {
-                'field': {
-                    'type': 'GOB.ManyReference',
-                    'ref': 'dst_catalogue:dst_collection'
-                }
-            }
-        }
-        mock_get_field_relations = lambda *args: [
-            {
-                'source': 'src_application',
-                'field_name': 'src_attr',
-                'destination_attribute': 'dst_attr',
-                'method': 'equals'
-            }
-        ]
-        expect = """
-            UPDATE
-                catalog_collection src
-            SET
-                field = new_values.field_updated
-            
-                FROM (
-                    --Select from all changed relations
-                    
-    SELECT * FROM (
-    
-    SELECT
-        src__id, src_volgnummer,
-        to_jsonb(array_to_json(array_agg(src_matchcolumn))) src_matchcolumn,
-        to_jsonb(array_to_json(array_agg(field_updated_elm))) field_updated
-    FROM (
-
-        SELECT --update specs
-            src__id, src_volgnummer,
-            dst__id, max(dst_volgnummer) dst_volgnummer,
-            src_matchcolumn,
-            
-            
-jsonb_set(
-jsonb_set(src_matchcolumn, '{volgnummer}', COALESCE(to_jsonb(max(dst_volgnummer)), 'null'::JSONB))
-, '{id}', COALESCE(to_jsonb(dst__id::TEXT), 'null'::JSONB))
-    field_updated_elm
-
-        FROM ( --relations
-            SELECT
-                CASE WHEN src._application = 'src_application' THEN 'equals' END AS method,
-                CASE WHEN src._application = 'src_application' THEN dst.dst_attr END AS match,
-                json_arr_elm AS src_matchcolumn,
-                
-                src._date_deleted AS src__date_deleted,
-    src._source AS src__source,
-    src._id AS src__id,
-    src.volgnummer AS src_volgnummer,
-    src.begin_geldigheid AS src_begin_geldigheid,
-    src.eind_geldigheid AS src_eind_geldigheid,
-    dst._date_deleted AS dst__date_deleted,
-    dst._source AS dst__source,
-    dst._id AS dst__id,
-    dst.volgnummer AS dst_volgnummer,
-    dst.begin_geldigheid AS dst_begin_geldigheid,
-    dst.eind_geldigheid AS dst_eind_geldigheid,
-    dst.dst_attr AS dst_match_dst_attr
-            FROM
-                catalog_collection AS src
-            
-JOIN jsonb_array_elements(src.field) AS json_arr_elm ON TRUE
-
-            
-            LEFT OUTER JOIN (
-                SELECT
-                    _date_deleted,
-    _source,
-    _id,
-    volgnummer,
-    begin_geldigheid,
-    eind_geldigheid,
-    dst_attr
-                FROM
-                    dst_catalogue_dst_collection) AS dst
-                ON
-                    (src._application = 'src_application' AND dst.dst_attr = json_arr_elm ->> 'bronwaarde') AND
-    (dst.begin_geldigheid < src.eind_geldigheid OR src.eind_geldigheid IS NULL) AND
-    (dst.eind_geldigheid >= src.eind_geldigheid OR dst.eind_geldigheid IS NULL)
-                WHERE
-                    (src._application = 'src_application' AND json_arr_elm IS NOT NULL AND json_arr_elm ->> 'bronwaarde' IS NOT NULL) AND (src._date_deleted IS NULL AND dst._date_deleted IS NULL)
-                ORDER BY
-                    src._source, src._id, src.volgnummer, src.begin_geldigheid, dst.begin_geldigheid, dst.eind_geldigheid
-        ) relations
-        GROUP BY
-            src__id, src_volgnummer,
-            src_matchcolumn,
-            dst__id
-            
-    
-    ) last_dsts
-    GROUP BY
-        src__id, src_volgnummer
-
-    ) _outer
-    WHERE --only select relations that have changed
-        src_matchcolumn != field_updated
-
-                    --Get changed relations in chunks
-                    ORDER BY
-                        src__id
-                    LIMIT
-                        50000
-                    OFFSET
-                        0
-                ) new_values
-                WHERE
-                    new_values.src__id = src._id AND new_values.src_volgnummer = src.volgnummer
-        """
-        def get_data_values():
-            yield {'count': 10}
-        mock_get_data.return_value = get_data_values()
-        with patch.object(GOBSources, 'get_field_relations', mock_get_field_relations), \
-             patch.object(GOBModel, 'get_collection', mock_get_collection), \
-             patch.object(GOBModel, 'has_states', lambda *args: True):
-            result = update_relations("catalog", "collection", "field")
-        mock_execute.assert_called_with(expect)
-
-        self.assertEqual(0, result)
-
-    @patch("gobupload.storage.relate.GOBModel")
-    @patch("gobupload.storage.relate.GOBSources")
-    def test_update_relations_verymany(self, mock_sources, mock_model):
-        model_inst = mock_model.return_value
-        mock_src_collection = {'all_fields': MagicMock()}
-        model_inst.get_collection.return_value = mock_src_collection
-        mock_src_collection['all_fields'].get.return_value = {'type': 'GOB.VeryManyReference'}
-
-        self.assertEqual(0, update_relations('catalog', 'collection', 'fiel'))
-
-        mock_sources.assert_not_called()
-
-    def test_get_updated_row_count(self):
-        self.assertEqual(_get_updated_row_count(0, 2), 0)
-        self.assertEqual(_get_updated_row_count(1, 2), 1)
-        self.assertEqual(_get_updated_row_count(2, 2), 2)
-
-        with self.assertRaises(RelateException):
-            _get_updated_row_count(3, 2)
 
     @patch('gobupload.storage.relate.get_relation_name')
     def test_get_relation_check_query_single(self, mock_get_relation_name):
