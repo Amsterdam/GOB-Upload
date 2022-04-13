6635f1c2340f059c45ef1797b1388ded69d10990
Roel Kramer <contact@roelkramer.nl>
2022-04-13 11:24:05 +0200

    Improve docker development env

--- src/test.sh
+++ src/test.sh
@@ -4,13 +4,13 @@ set -u # crash on missing env
 set -e # stop on any error
 
 # Clear any cached results
-find . -name "*.pyc" -exec rm -f {} \;
+#find . -name "*.pyc" -exec rm -f {} \;
+
+echo "Running tests"
+coverage run --source=./gobupload -m pytest tests/
+
+echo "Running coverage report"
+coverage report --show-missing --fail-under=93
 
 echo "Running style checks"
 flake8
-
-echo "Running unit tests"
-pytest tests/
-
-echo "Running coverage tests"
-pytest --cov=gobupload --cov-report html --cov-fail-under=93
