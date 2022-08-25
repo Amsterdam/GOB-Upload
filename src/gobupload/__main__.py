4612a43d9ee078339f7e3a493ebfa7890d89a825
Roel Kramer <contact@roelkramer.nl>
2022-08-25 13:49:20 +0200

    Fix imports

--- src/gobupload/__main__.py
+++ src/gobupload/__main__.py
@@ -6,10 +6,7 @@ It writes the storage to apply events to the storage
 
 """
 import argparse
-import json
 import sys
-from pathlib import Path
-from typing import Any, Optional, Dict, Callable
 
 from gobcore.message_broker.config import COMPARE_RESULT_KEY, FULLUPDATE_RESULT_KEY, \
  APPLY_RESULT_KEY, \
@@ -20,9 +17,7 @@ from gobcore.message_broker.config import WORKFLOW_EXCHANGE, FULLUPDATE_QUEUE, \
  RELATE_PREPARE_QUEUE, RELATE_PROCESS_QUEUE, RELATE_CHECK_QUEUE, \
  RELATE_UPDATE_VIEW_QUEUE
 from gobcore.message_broker.messagedriven_service import messagedriven_service
-from gobcore.message_broker.offline_contents import load_message, offload_message
-from gobcore.message_broker.utils import to_json, from_json
-
+from gobcore.standalone import run_as_standalone, parent_argument_parser
 from gobupload import apply
 from gobupload import compare
 from gobupload import relate
@@ -30,8 +25,6 @@ from gobupload import update
 from gobupload.storage.handler import GOBStorageHandler
 from gobupload.config import DEBUG
 
-
-Message = dict[str, Any]
 
 SERVICEDEFINITION = {
     'apply': {  # in workflow this is called update_model
@@ -94,24 +87,7 @@ SERVICEDEFINITION = {
 
 
 def argument_parser():
-    parser = argparse.ArgumentParser(
-        description='Start standalone GOB Tasks',
-    )
-    parser.add_argument(
-         "--message-data",
-         default=json.dumps({}),
-         help="Message data used by the handler."
-     )
-    parser.add_argument(
-        "--message-result-path",
-        default="/airflow/xcom/return.json",
-        help="Path to store result message."
-    )
-    subparsers = parser.add_subparsers(
-        title="subcommands",
-        help="Which handler to run.",
-        dest="handler",
-    )
+    parser, subparsers = parent_argument_parser()
     # Apply handler parser (also called upload_model in workflow)
     apply_parser = subparsers.add_parser(
         name="apply",
@@ -181,83 +157,6 @@ if DEBUG:
     print("WARNING: Debug mode is ON")
 
 
-def _write_message(message_out: Dict[str, Any], write_path: Path) -> None:
-    """Write message data to a file. Ensures parent directories exist.
-    :param message_out: Message data to be written
-    :param write_path: Path to write message data to. To use airflow's xcom,
-        use `/airflow/xcom/return.json` as a path.
-    """
-    print(f"Writing message data to {write_path}")
-    write_path.parent.mkdir(parents=True, exist_ok=True)
-    write_path.write_text(json.dumps(message_out))
-
-
-def build_message(args: argparse.Namespace) -> Message:
-    """Create a message from argparse arguments.
-
-    Defaults to None if attribute has no value.
-
-    :param args: Parsed arguments
-    :return: A message with keys as required by different handlers.
-    """
-    return {
-        'header': {
-            'catalogue': getattr(args, "catalogue", None),
-            'mode': getattr(args, "mode", None),
-            'collection': getattr(args, "collection", None),
-            'entity': getattr(args, "collection", None),
-            'attribute': getattr(args, "attribute", None)
-        }
-    }
-
-
-def get_handler(handler: str, mapping: Dict[str, Any]) -> Callable:
-    """Returns handler from a dictionary which is formatted like:
-
-    {
-        "handler_name": {
-            "handler": some_callable
-        }
-    }
-
-    This mapping usually is SERVICEDEFINITION.
-
-    :param handler: name of the handler to lookup in the mapping.
-    :param mapping: mapping formatted as described above.
-    :returns: A callable.
-    """
-    return mapping.get(handler)["handler"]
-
-
-def run_as_standalone(
-        args: argparse.Namespace, service_definition: dict[str, Any]
-) -> Optional[Message]:
-    message = build_message(args)
-    storage = GOBStorageHandler()
-    storage.init_storage()
-
-    print(f"Loading incoming message: {message}")
-    # Load offloaded 'contents_ref'-data into message
-    message_in, offloaded_filename = load_message(
-        msg=message,
-        converter=from_json,
-        params={"stream_contents": False}
-    )
-    print("Fully loaded incoming message, including data:")
-    print(message_in)
-    handler = get_handler(args.handler, service_definition)
-    message_out = handler(message_in)
-    message_out_offloaded = offload_message(
-        msg=message_out,
-        converter=to_json,
-        force_offload=True
-    )
-
-    print(f"Writing message data to {args.message_result_path}")
-    _write_message(message_out_offloaded, Path(args.message_result_path))
-    return message_out_offloaded
-
-
 def run_as_message_driven() -> None:
     """Run in message driven mode, listening to a message queue."""
     storage = GOBStorageHandler()
@@ -280,18 +179,19 @@ def main():
         print("Arguments found, run as standalone")
         parser = argument_parser()
         args = parser.parse_args()
-
+        storage = GOBStorageHandler()
         # Special case for migrate, which is specific to upload
         if args.handler == "migrate":
             recreate = [args.mv_name] \
                 if args.materialized_views and args.mv_name else args.materialized_views
-            storage = GOBStorageHandler()
+
             storage.init_storage(
                 force_migrate=True,
                 recreate_materialized_views=recreate
             )
             return
 
+        storage.init_storage()
         run_as_standalone(args, SERVICEDEFINITION)
 
 
