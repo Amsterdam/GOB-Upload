54ef1761731db498328745cb57e3c014e36a5d5a
Roel de Vries <roel.de.vries@amsterdam.nl>
2023-01-06 16:55:44 +0100

    Rework storing events and EventCollector
    - Drop ActiveGarbageCollector
    - Use sqlalchemy bulk insert
    - Use 1 transaction for all events processed
    - insert events in chunks of 10000
    - Remove BULKCONFIRM logic

--- src/gobupload/update/event_collector.py
+++ src/gobupload/update/event_collector.py
@@ -3,85 +3,64 @@ Event Collector
 
 Stores events in the event table
 """
+from gobcore.exceptions import GOBException
+from gobcore.logging.logger import logger
+from gobupload.storage.handler import GOBStorageHandler
 
 
 class EventCollector:
 
-    MAX_CHUNK = 10000
-
-    def __init__(self, storage, last_events):
-        self.storage = storage
+    def __init__(self, storage: GOBStorageHandler, last_events: dict[str, str]):
         # Local dictionary that contains the last event number for every source_id
         self.last_events = last_events
         self.events = []
+        self.storage = storage
 
     def __enter__(self):
-        self.events = []
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.store_events()
-
-    def add_event(self, event):
-        if event['event'] == 'BULKCONFIRM':
-            # Bulk events are stored immediately
-            return self.storage.add_events([event])
-
-        self.events.append(event)
-        if len(self.events) >= self.MAX_CHUNK:
-            # All other events are grouped per 10000
-            return self.store_events()
+        if self.events:
+            raise GOBException("Have not stored events. Call store_events() before leaving context")
 
     def store_events(self):
-        if len(self.events):
+        if self.events:
             self.storage.add_events(self.events)
-            self.events = []
+            self.events.clear()
 
     def collect(self, event):
         """
         Checks an event for being valid and stores it in the events table
 
         :param event:
-        :return: True is the event was valid and stored
+        :return:
         """
-        is_valid = self._validate(event)
-        if is_valid:
-            self.add_event(event)
-        return is_valid
+        self.events.append(event)
 
-    def _match_last_event(self, id, event_type):
+    def _match_last_event(self, id_, event_type) -> bool:
         """
         Tells if an event matches with the last event of the corresponding entity
 
         The compare step tells the last event of the entity against which is compared
         If this matches with the current last event of the entity the event is valid
-        :param id:
+        :param id_:
         :param event_type:
         :return:
         """
-        last_event = self.last_events.get(id['tid'])
-        return id['last_event'] == last_event or (event_type == 'ADD' and last_event is None)
+        last_event = self.last_events.get(id_['tid'])
+        return id_['last_event'] == last_event or (event_type == 'ADD' and last_event is None)
 
-    def _validate(self, event):
+    def is_valid(self, event) -> bool:
         """
         Tells if an event is valid by matching against the last event of the corresponding entity
 
         :param event:
         :return:
         """
-        event_type = event['event']
-        if event_type == 'BULKCONFIRM':
-            ids = [{
-                'tid': confirm['_tid'],
-                'last_event': confirm['_last_event']
-            } for confirm in event['data']['confirms']]
-        else:
-            ids = [{
-                'tid': event['data']['_tid'],
-                'last_event': event['data']['_last_event']
-            }]
+        obj = {"tid": event["data"]["_tid"], "last_event": event["data"]["_last_event"]}
+        is_valid = self._match_last_event(obj, event["event"])
 
-        is_valid = all([self._match_last_event(id, event_type) for id in ids])
         if not is_valid:
-            print("Invalid event", event, [self.last_events.get(id['tid']) for id in ids])
+            logger.error("Invalid event", event, self.last_events.get(obj["tid"]))
+
         return is_valid
