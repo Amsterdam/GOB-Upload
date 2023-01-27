e65a273e13cfe2cec69a63a2f1abf700d0079013
Roel de Vries <roel.de.vries@amsterdam.nl>
2023-01-27 16:56:12 +0100

    Rework temporary table to contextmanager

--- src/gobupload/compare/entity_collector.py
+++ src/gobupload/compare/entity_collector.py
@@ -14,7 +14,12 @@ class EntityCollector:
         """
         self.storage = storage
         self._entities = []
-        self.storage.create_temporary_table()
+        storage.create_temporary_table()
+
+    def _write_entities(self):
+        if self._entities:
+            self.storage.write_temporary_entities(self._entities)
+            self._entities.clear()
 
     def collect(self, entity):
         """
@@ -25,9 +30,8 @@ class EntityCollector:
         self._entities.append(entity)
 
         if len(self._entities) >= 10_000:
-            self.storage.write_temporary_entities(self._entities)
-            self._entities.clear()
+            self._write_entities()
 
     def close(self):
-        if self._entities:
-            self.storage.write_temporary_entities(self._entities)
+        self._write_entities()
+        self.storage.analyze_temporary_table()
